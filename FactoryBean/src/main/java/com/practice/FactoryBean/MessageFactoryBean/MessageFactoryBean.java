28b43d17a9e6f53eeb2e0dba241c46d7e9c2fb6c
neutron <chosh901@naver.com>
2023-09-14 22:56:57 +0900

    Factory Bean practice code

--- FactoryBean/src/main/java/com/practice/FactoryBean/MessageFactoryBean/MessageFactoryBean.java
+++ FactoryBean/src/main/java/com/practice/FactoryBean/MessageFactoryBean/MessageFactoryBean.java
@@ -2,9 +2,14 @@ package com.practice.FactoryBean.MessageFactoryBean;
 
 import com.practice.FactoryBean.Message.Message;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MessageFactoryBean  implements FactoryBean<Message> {
 
+    @Value("factory bean sample test")
     String text;
 
 
--- FactoryBean/src/test/java/com/practice/FactoryBean/FactoryBeanApplicationTests.java
+++ FactoryBean/src/test/java/com/practice/FactoryBean/FactoryBeanApplicationTests.java
@@ -1,13 +1,22 @@
 package com.practice.FactoryBean;
 
+import com.practice.FactoryBean.Message.Message;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class FactoryBeanApplicationTests {
 
+	@Autowired
+	Message message;
+
 	@Test
-	void contextLoads() {
+	void messageBeanTest() {
+		String rtn = message.getText();
+		assertTrue(rtn.equals("factory bean sample test"));
 	}
 
 }
