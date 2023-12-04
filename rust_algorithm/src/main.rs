1dbcb51fa9e7e6c7c6879cadee18b7eba3f2ae8a
neutron <chosh901@naver.com>
2023-12-05 08:31:23 +0900

    rust algorithm

--- rust_algorithm/src/main.rs
+++ rust_algorithm/src/main.rs
@@ -2,25 +2,15 @@ mod stack_queue_deque;
 use std::io;
 
 fn main() {
+
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input  = buf.split_ascii_whitespace();
     
-    let mut n = String::new();
-    io::stdin().read_line(&mut n).unwrap();
-    let n:usize = n.trim().parse().unwrap();
-
-    let mut is_queue = String::new();
-    io::stdin().read_line(&mut is_queue).unwrap();
-    let is_queue:Vec<u32> = is_queue.trim().split_whitespace().map(|s| s.parse().unwrap()).collect();
-
-    let mut store= String::new();
-    io::stdin().read_line(&mut store).unwrap();
-    let store:Vec<u32> = store.trim().split_whitespace().map(|s| s.parse().unwrap()).collect();
-
-    let mut input= String::new();
-    io::stdin().read_line(&mut input).unwrap();
-
-    let mut input= String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let input:Vec<u32> = input.trim().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let n:usize = input.next().unwrap().parse().unwrap();
+    let is_queue:Vec<u32> = (0..n).map(|_| input.next().unwrap().parse().unwrap()).collect();
+    let store:Vec<u32> = (0..n).map(|_| input.next().unwrap().parse().unwrap()).collect();
+    let m:usize = input.next().unwrap().parse().unwrap();
+    let input:Vec<u32> = (0..m).map(|_| input.next().unwrap().parse().unwrap()).collect();
 
     let rtn = stack_queue_deque::queuestack::solve(n, is_queue, store, input);
     for e in rtn {
