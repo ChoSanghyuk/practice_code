38631622c14904bb9e2f14e42e703ea72a3ea389
cnssh <chosh901@naver.com>
2024-03-06 08:16:59 +0900

    update git.sh code

--- git_code/git.sh
+++ git_code/git.sh
@@ -2,11 +2,11 @@
 
 git pull origin master
 
-commit_message=""
-for i in $@
-do
-commit_message="$commit_message $i"
-done
+commit_message="$@"
+#for i in $@
+#do
+#commit_message="$commit_message $i"
+#done
 
 git add .
 git commit -m "$commit_message"
