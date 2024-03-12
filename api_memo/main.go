93f652ea30f47e666b2689ab7aca93f4a4868b3d
cnssh <chosh901@naver.com>
2024-03-12 12:49:56 +0900

    go api

--- api_memo/main.go
+++ api_memo/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Post struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
 func messageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, this is the message from the API!")
+
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, "Hello, this is the message from the API!")
+	case "POST":
+		var newPost Post
+		err := json.NewDecoder(r.Body).Decode(&newPost)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		} else {
+			fmt.Printf("%v\r", newPost)
+		}
+	}
+
 }
 
 func main() {
@@ -15,4 +36,5 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
+	fmt.Printf("Server Started")
 }
