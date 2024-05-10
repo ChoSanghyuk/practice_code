288cbfcd66a9d9dfdffab71005103a9672b9c91f
조상혁 <sh@josanghyeogs-MacBook-Pro.local>
2024-05-10 01:36:33 -0700

    go fiber

--- go_fiber/main.go
+++ go_fiber/main.go
@@ -5,8 +5,8 @@ import "github.com/gofiber/fiber/v3"
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Get("/:name.:age", func(c fiber.Ctx) error {
+		return c.SendString("Hello!" + c.Params("name") + "   " + c.Params("age")) //+
 	})
 
 	app.Listen(":3000")
