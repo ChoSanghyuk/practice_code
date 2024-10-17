c8a74b07e10e350a7ebb304d38e37e10270e1417
unknown <s1214396@miraeasset.com>
2024-10-17 11:05:46 +0900

    upgradeable

--- besu_go_module/smart_contract/upgradeable/implement/MyImplementV1.sol
+++ besu_go_module/smart_contract/upgradeable/implement/MyImplementV1.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.1 <0.9.0;
 
-import "@openzeppelin/contracts-upgradealbe/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradealbe/proxy/utils/Initializable.sol";
+import {AccessControlUpgradeable} "@openzeppelin/contracts-upgradealbe/access/AccessControlUpgradeable.sol";
+import {Initializable} "@openzeppelin/contracts-upgradealbe/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol"
 
-contract MyImplementV1 is Initializable, AccessControlUpgradeable {
+contract MyImplementV1 is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
 
     bytes32 public constant CUSTOM_ROLE = keccak256("CUSTOM_ROLE");
 
--- besu_go_module/smart_contract/upgradeable/proxy/MyProxy.sol
+++ besu_go_module/smart_contract/upgradeable/proxy/MyProxy.sol
@@ -1,10 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.1 <0.9.0;
 
-import "@openzeppelin/contracts/proxy/Proxy.sol";
-import "@openzeppelin/contracts/proxy/ERC1967Upgrade.sol";
+import {ERC1967Proxy} "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract MyProxy is Proxy {
+/*
+UUPS proxies are implemented using an {ERC1967Proxy}. Note that this proxy is not by itself upgradeable. It is the role of the implementation to include, alongside the contract’s logic, all the code necessary to update the implementation’s address that is stored at a specific slot in the proxy’s storage space. This is where the {UUPSUpgradeable} contract comes in. Inheriting from it (and overriding the {xref-UUPSUpgradeable-_authorizeUpgrade-address-}[_authorizeUpgrade] function with the relevant access control mechanism) will turn your contract into a UUPS compliant implementation.
+*/
+
+/*
+Requirements:
+     *
+     * - If `data` is empty, `msg.value` must be zero.
+*/
+
+/*
+UUPS와 Beacon은 같이 사용하지 않음. 서로의 목적이 다르기 때문.
+UUPS는 implement에서 업그레이드 로직을 관리하기 위함. Beacon은 한번의 작업으로 모든 프록시들의 Implemet 주소를 바꾸기 위함
+*/
+
+contract MyProxy is ERC1967Proxy {
     
     constructor(address newImplementation) {
         _upgradeTo(newImplementation);
